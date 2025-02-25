32696b262c08ced5aaac843f24d89552fa8c1869
Vicky Kumar <imvickykumar999@gmail.com>
2025-02-25 13:08:26 +0530

    Update settings.py

--- workflow_project/workflow_project/settings.py
+++ workflow_project/workflow_project/settings.py
@@ -29,9 +29,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', ]
 
 
 # Application definition
-INSTALLED_APPS = [
-    'jazzmin',
-    
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
